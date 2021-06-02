842f55a9ecc09420efd98b65965e18e000bb62cc
Dimasta Ki <33998442+DimastaKi@users.noreply.github.com>
2021-06-02 13:39:36 +0300

    Add files via upload

--- metalsvist.py
+++ metalsvist.py
@@ -32,6 +32,98 @@ import shutil
      # ------------------------------------------
      # | DIN471 | 95PK1 | Стоп.кольцо внешн.471 |
 
+# Словарь соотношений ГОСТа в DIN + ISO
+dic_for_standarts = {
+     "397" : "DIN 94, ISO 1234",
+     "1144" : "DIN 96, DIN 7981, ISO 7049",
+     "1145" : "DIN 7982, DIN 97, ISO 7050",
+     "1146" : "DIN 95, DIN 7983, ISO 7051",
+     "1476" : "DIN 553, ISO 7434",
+     "1477" : "DIN 438, DIN 551, ISO 7436, ISO 4766",
+     "1478" : "DIN 417, ISO 7435",
+     "1481" : "DIN 561",
+     "1482" : "DIN 479",
+     "1485" : "DIN 479",
+     "1486" : "DIN 480",
+     "1488" : "DIN 478",
+     "1491" : "DIN 84, ISO 1207",
+     "3032" : "DIN 315",
+     "3033" : "DIN 444",
+     "3057" : "DIN 2093",
+     "3070" : "DIN 3060",
+     "3128" : "DIN 7, DIN 6325, ISO 2338",
+     "3129" : "DIN 1, ISO 2339",
+     "4751" : "DIN 580, ISO 3266",
+     "5915" : "DIN 555, DIN 934, ISO 4034, ISO 4032, ISO 8673",
+     "5916" : "DIN 439, DIN 936, ISO 4035, ISO 4036, ISO 8675",
+     "5918" : "DIN 935, ISO 7035, ISO 7036, ISO 7037",
+     "5919" : "DIN 937, ISO 7038",
+     "5927" : "DIN 555, DIN 934, ISO 4032, ISO 4034, ISO 8673",
+     "5932" : "DIN 935, DIN 937, ISO 7035, ISO 7036, ISO 7037, ISO 7038",
+     "6393" : "DIN 1816",
+     "6402" : "DIN 127",
+     "6958" : "DIN 440, DIN 9021, ISO 7094, ISO 7093",
+     "7798" : "DIN 931, DIN 933, ISO 4014",
+     "7801" : "DIN 607",
+     "7802" : "DIN 603, ISO 8677",
+     '7805' : "DIN 931, DIN 933, ISO 4014",
+     "8878" : "DIN 914, ISO 4027",
+     "9464" : "DIN 7978, ISO 8736",
+     "9649" : "DIN 125, ISO 7089, ISO 7090",
+     "10299" : "DIN 660, ISO 1051",
+     "10300" : "DIN 661, ISO 1051",
+     "10301" : "DIN 662, ISO 1051",
+     "10302" : "DIN 674, ISO 1051",
+     "10337" : "DIN 7964",
+     "10338" : "DIN 7964",
+     "10450" : "DIN 433, ISO 7092",
+     "10462" : "DIN 6797, DIN 6798",
+     "10463" : "DIN 6798",
+     "10464" : "DIN 6797, DIN 6798",
+     "10619" : "DIN 7982, ISO 7050",
+     "10620" : "DIN 7983, ISO 7051",
+     "10621" : "DIN 7516 A, DIN 7981, ISO 7049",
+     "10657" : "DIN 546",
+     "10906" : "DIN 434, DIN 435",
+     "11074" : "DIN 913, ISO 4026",
+     "11075" : "DIN 915, ISO 4028",
+     "11371" : "DIN 125, ISO 7089, ISO 7090",
+     "11473" : "DIN 571",
+     "11644" : "DIN 967",
+     "11648" : "DIN 6799",
+     "11738" : "DIN 912, DIN 7984, ISO 4762, ISO 21269",
+     "11860" : "DIN 1587",
+     "11871" : "DIN 546, DIN 1804",
+     "11872" : "DIN 5406",
+     "13152" : "DIN 186",
+     "13438" : "DIN 6319",
+     "13439" : "DIN 6796",
+     "13463" : "DIN 93, DIN 463",
+     "13464" : "DIN 93, DIN 463",
+     "13942" : "DIN 471",
+     "13943" : "DIN 472",
+     "14229" : "DIN 1481, ISO 8752",
+     "14724" : "DIN 444",
+     "14725" : "DIN 444",
+     "15522" : "DIN 431",
+     "15523" : "DIN 6330",
+     "17473" : "DIN 85, DIN 7985, ISO 1580, ISO 7045",
+     "17474" : "DIN 964, DIN 966, ISO 2010, ISO 7047",
+     "17475" : "DIN 963, DIN 965, ISO 2009, ISO 7046",
+     "17673" : "DIN 605",
+     "18746" : "DIN 427, ISO 2342",
+     "22033" : "DIN 938",
+     "22034" : "DIN 939",
+     "22035" : "DIN 939",
+     "22038" : "DIN 835",
+     "22353" : "DIN 6914, ISO 7412",
+     "22354" : "DIN 6915, ISO 7414",
+     "22355" : "DIN 6916, ISO 7416",
+     "23360" : "DIN 6885",
+     "24071" : "DIN 6888, ISO 3912",
+     "28964" : "DIN 916, ISO 4029",
+}
+
 # рисует линию
 def line():
     print ("{0:=^60}".format("> METALL-SVIST <"))
@@ -101,97 +193,7 @@ def module_gost2din():
     print("\n" + "{0:>^16} Преобразование гостов в din {0:<^16}".format("").upper())
     gost = input("Введите номер ГОСТа >> ")
 
-    # Словарь соотношений ГОСТа в DIN + ISO
-    dic_for_standarts = {
-         "397" : "DIN 94, ISO 1234",
-         "1144" : "DIN 96, DIN 7981, ISO 7049",
-         "1145" : "DIN 7982, DIN 97, ISO 7050",
-         "1146" : "DIN 95, DIN 7983, ISO 7051",
-         "1476" : "DIN 553, ISO 7434",
-         "1477" : "DIN 438, DIN 551, ISO 7436, ISO 4766",
-         "1478" : "DIN 417, ISO 7435",
-         "1481" : "DIN 561",
-         "1482" : "DIN 479",
-         "1485" : "DIN 479",
-         "1486" : "DIN 480",
-         "1488" : "DIN 478",
-         "1491" : "DIN 84, ISO 1207",
-         "3032" : "DIN 315",
-         "3033" : "DIN 444",
-         "3057" : "DIN 2093",
-         "3070" : "DIN 3060",
-         "3128" : "DIN 7, DIN 6325, ISO 2338",
-         "3129" : "DIN 1, ISO 2339",
-         "4751" : "DIN 580, ISO 3266",
-         "5915" : "DIN 555, DIN 934, ISO 4034, ISO 4032, ISO 8673",
-         "5916" : "DIN 439, DIN 936, ISO 4035, ISO 4036, ISO 8675",
-         "5918" : "DIN 935, ISO 7035, ISO 7036, ISO 7037",
-         "5919" : "DIN 937, ISO 7038",
-         "5927" : "DIN 555, DIN 934, ISO 4032, ISO 4034, ISO 8673",
-         "5932" : "DIN 935, DIN 937, ISO 7035, ISO 7036, ISO 7037, ISO 7038",
-         "6393" : "DIN 1816",
-         "6402" : "DIN 127",
-         "6958" : "DIN 440, DIN 9021, ISO 7094, ISO 7093",
-         "7798" : "DIN 931, DIN 933, ISO 4014",
-         "7801" : "DIN 607",
-         "7802" : "DIN 603, ISO 8677",
-         '7805' : "DIN 931, DIN 933, ISO 4014",
-         "8878" : "DIN 914, ISO 4027",
-         "9464" : "DIN 7978, ISO 8736",
-         "9649" : "DIN 125, ISO 7089, ISO 7090",
-         "10299" : "DIN 660, ISO 1051",
-         "10300" : "DIN 661, ISO 1051",
-         "10301" : "DIN 662, ISO 1051",
-         "10302" : "DIN 674, ISO 1051",
-         "10337" : "DIN 7964",
-         "10338" : "DIN 7964",
-         "10450" : "DIN 433, ISO 7092",
-         "10462" : "DIN 6797, DIN 6798",
-         "10463" : "DIN 6798",
-         "10464" : "DIN 6797, DIN 6798",
-         "10619" : "DIN 7982, ISO 7050",
-         "10620" : "DIN 7983, ISO 7051",
-         "10621" : "DIN 7516 A, DIN 7981, ISO 7049",
-         "10657" : "DIN 546",
-         "10906" : "DIN 434, DIN 435",
-         "11074" : "DIN 913, ISO 4026",
-         "11075" : "DIN 915, ISO 4028",
-         "11371" : "DIN 125, ISO 7089, ISO 7090",
-         "11473" : "DIN 571",
-         "11644" : "DIN 967",
-         "11648" : "DIN 6799",
-         "11738" : "DIN 912, DIN 7984, ISO 4762, ISO 21269",
-         "11860" : "DIN 1587",
-         "11871" : "DIN 546, DIN 1804",
-         "11872" : "DIN 5406",
-         "13152" : "DIN 186",
-         "13438" : "DIN 6319",
-         "13439" : "DIN 6796",
-         "13463" : "DIN 93, DIN 463",
-         "13464" : "DIN 93, DIN 463",
-         "13942" : "DIN 471",
-         "13943" : "DIN 472",
-         "14229" : "DIN 1481, ISO 8752",
-         "14724" : "DIN 444",
-         "14725" : "DIN 444",
-         "15522" : "DIN 431",
-         "15523" : "DIN 6330",
-         "17473" : "DIN 85, DIN 7985, ISO 1580, ISO 7045",
-         "17474" : "DIN 964, DIN 966, ISO 2010, ISO 7047",
-         "17475" : "DIN 963, DIN 965, ISO 2009, ISO 7046",
-         "17673" : "DIN 605",
-         "18746" : "DIN 427, ISO 2342",
-         "22033" : "DIN 938",
-         "22034" : "DIN 939",
-         "22035" : "DIN 939",
-         "22038" : "DIN 835",
-         "22353" : "DIN 6914, ISO 7412",
-         "22354" : "DIN 6915, ISO 7414",
-         "22355" : "DIN 6916, ISO 7416",
-         "23360" : "DIN 6885",
-         "24071" : "DIN 6888, ISO 3912",
-         "28964" : "DIN 916, ISO 4029",
-    }
+    
 
     # получение ключа-госта из введенного пользователем
     temp_keys = ""
@@ -311,14 +313,46 @@ def module_din_in_metalvis_id():
             print ("\nВведеный DIN/ISO: " + str(input_standart) + ".\nНет в базе Metalvis или ошибка написания\n")
             open_dict.close()
 
+#Модуль администратора
 def module_admin():
     print ("{0:=^60}".format("> ADMIN PANEL <"))
     print("\n\
-    id: Добавление новых пар значений в словарь поиска артикулов Metalvis\n\
+    id: Добавление новых пар значений в словарь поиска артикулов Metalvis\n\n\
+    1: Вывести список SKU\n\
+    2: Вывести список пар ГОСТ:DIN(ISO)\n\
     e: Выйти в главное меню\n")
 
     module_start = input("Выберите раздел: ")
+    
+    #Запуск раздела вывода списка SKU из файла dict_metalvis_id
+    if module_start == "1":
 
+    # открывает словарь/лист для поиска значения
+        open_dict = (open(str(parent) + "/dict_metalvis_id.py", "r"))
+        read_dict_file = open_dict.readline()
+        dict_info = str(eval(read_dict_file))
+        dict_info = dict_info.replace("', ","\n")
+        dict_info = dict_info.replace("'","")
+        dict_info = dict_info.replace("{","")
+        dict_info = dict_info.replace("}","")
+        dict_info = dict_info.replace("DIN","DIN ")
+        dict_info = dict_info.replace("ISO","ISO ")
+        
+        print ("\nВывод всего списка пар DIN\ISO:SKU\n" + dict_info + "\n")
+     
+    # Запуск раздела вывода пары ГОСТ:DIN(ISO)
+    if module_start == "2":
+        get_gost = str(dic_for_standarts.items())
+        get_gost = get_gost.replace("),","\n")
+        get_gost = get_gost.replace("(","ГОСТ ")
+        get_gost = get_gost.replace("'","")
+        get_gost = get_gost.replace(")])","")
+        get_gost = get_gost.replace("dict_itemsГОСТ ["," ")
+        os.system('clear')
+        print ("\nСписок всех пар ГОСТ:DIN\ISO\n" + get_gost + "\n")
+        
+    
+# Запуск раздела добавление нового SKU
     if module_start == "id":
         print("Пример записи:'DIN931':'56600 Болт'")
         selected_standard = input("Что вы хотите добавить:\n\
@@ -429,7 +463,6 @@ def module_fine_pitch_fasteners():
         get_thread_pitch = get_thread_pitch.replace(" ","")
         convert_to_list_of_thread_pitch = list(get_thread_pitch.split(","))
         print ("")
-        
         count_of_thread_pitch = list(get_thread_pitch.split(","))
         len_of_thread_pitch = (len(count_of_thread_pitch))-1
         if len_of_thread_pitch == 0:
